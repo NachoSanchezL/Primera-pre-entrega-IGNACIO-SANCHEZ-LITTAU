7ca1e24e77296ec904d82bf5620c1cd9a04368cc
IgnacioSanchezLittau <sanchezlittauignacio@gmail.com>
2022-12-29 15:35:05 -0300

    inicio de primer entrega

---
+++ main.js
@@ -0,0 +1,13 @@
+const  costoFernet = 1800;
+const costoCerveza = 500;
+const costoVino = 800;
+
+let costoFinal = 0;
+
+let cantidadDeFernet = 0 ;
+let cantidadDeCerveza = 0 ;
+let cantidadDeVino = 0 ;
+
+cantidadDeCerveza = Number (prompt("Ingrese cantidad de Cerveza/s que quiere")) ;
+cantidadDeFernet= Number (prompt("Ingrese cantidad de Fernet/s que quiere")) ;
+cantidadDeVino= Number (prompt("Ingrese cantidad de Vino/s que quiere")) ;
